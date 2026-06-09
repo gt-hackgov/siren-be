40bc8435639bae4e2baf2f1c6342adbc672bca40
gabcrvlh <gabrieladcrvlh@gmail.com>
2026-06-09 17:02:34 -0300

    cria SirenApplication

---
+++ src/main/java/com/siren/app/SirenApplication.java
@@ -0,0 +1,13 @@
+package com.siren.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SirenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SirenApplication.class, args);
+	}
+
+}
